43b3a74e53170591bb51d8c77bf9b610190d2635
Winton Wedderburn <masterkong@ubuntu-20.04-lts>
2020-11-02 20:57:39 -0500

    Items can be saved to the database.

---
+++ models/items.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    price: {type: Number, default: 0},
+    category: {type: String, required: true, default: 'Uncategorized'},
+    make: String,
+    model: String,
+    item_id: String,
+    location: String,
+    units: Number, 
+    insured: Boolean
+});
+
+const Items = mongoose.model('Item', itemSchema);
+
+module.exports = Items;
--- server.js
+++ server.js
@@ -1,54 +1,90 @@
-//___________________
 //Dependencies
-//___________________
-const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const app = express ();
-const db = mongoose.connection;
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3000;
 
-//___________________
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const app = express();
+
+
+//Port
+
+// Allow use of Heroku's port or your own local port, depending on the environment
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+
 //Database
-//___________________
+
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'project2';
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'project2';
 
 // Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true
+// });
+
+// DATA
+
+const Item = require('./models/items.js');
+
+//mongoose
+// mongoose.connect('mongodb:localhost:27017/project2', {useNewUrlParser: true, useUnifiedTopology: true}); 
+// mongoose.connection.once('open', () => {
+//   console.log('connected to mongo');
+// });
+const mongoURI = 'mongodb://localhost:27017/' + 'project2';
+const db = mongoose.connection;
+
+mongoose.connect(mongoURI);
+
+// Deprecation warning
+mongoose.connect(mongoURI, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, () => {
+  console.log('the connection with mongod is established');
+});
+
+
+
 
 // Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+// db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+// db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+// db.on('disconnected', () => console.log('mongo disconnected'));
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', mongoURI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-// open the connection to mongo
-db.on('open' , ()=>{});
 
-//___________________
-//Middleware
-//___________________
+
+// open the connection to mongo
+// db.on('open', () => {});
+
+
+//MIDDLEWARE
+
 
 //use public folder for static assets
 app.use(express.static('public'));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+app.use(express.urlencoded({
+  extended: false
+})); // extended: false - does not allow nested objects in query strings
+app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+app.use(methodOverride('_method')); // allow POST, PUT and DELETE from a form
 
 
-//___________________
+
+
 // Routes
-//___________________
+
 //localhost:3000
-app.get('/' , (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
   console.log(`this is the index route`);
 });
@@ -62,26 +98,36 @@ app.get('/new', (req, res) => {
 
 //create
 app.post('/', (req, res) => {
-      res.send(`create route`);
-      console.log(`this is the create route`);
+  if (req.body.insured === 'on') {
+    req.body.insured = true;
+  } else {
+    req.body.insured = false;
+  }
+
+  Item.create(req.body, (error, createdItem) => {
+    res.send(createdItem);
+    console.log(req.body, createdItem);
+  });
+  // res.redirect('/');
+  
 });
 
 // update
 app.put('/:id', (req, res) => {
-      res.send(`update route`);
-      console.log(`this is the update route`);
+  res.send(`update route`);
+  console.log(`this is the update route`);
 });
 
 // edit
 app.get('/:id/edit', (req, res) => {
-        res.send(`edit route`);
-        console.log(`this is the edit route`);
+  res.send(`edit route`);
+  console.log(`this is the edit route`);
 });
 
 // show
 app.get('/:id', (req, res, next) => {
-      res.send(`show route`);
-      console.log(`this is the show route`);
+  res.send(`show route`);
+  console.log(`this is the show route`);
 });
 
 // delete
@@ -90,7 +136,7 @@ app.delete('/:id', (req, res) => {
   console.log(`this is the delete route`);
 });
 
-//___________________
+
 //Listener
-//___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+
+app.listen(PORT, () => console.log('Listening on port:', PORT));
